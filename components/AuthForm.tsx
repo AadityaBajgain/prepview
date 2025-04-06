380816631e423006d5d645560d575119a964a645
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-04-06 16:20:24 -0700

    refactor: enhance AuthForm with FormField component and improve user feedback

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -14,6 +14,8 @@ import SignIn from "@/app/(auth)/sign-in/page"
 import Link from "next/link"
 import { sign } from "crypto"
 import {toast} from "sonner";
+import FormField from "./FormField"
+import { useRouter } from "next/navigation"
 
 
 const authFormSchema = (type:FormType)=>{
@@ -25,6 +27,7 @@ const authFormSchema = (type:FormType)=>{
 }
 
 const AuthForm = ({type}:{type:FormType}) => {
+  const router = useRouter();
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -39,9 +42,12 @@ const AuthForm = ({type}:{type:FormType}) => {
     try{
       if(type === "sign-up")
       {
-        console.log("Sign up: ", values);
+        toast.success("Account Created Successfully. Please Sign in.")
+        router.push('/sign-in');
       }else{
-        console.log("Sign in: ", values);      }
+        toast.success("Sign in Successfully.")
+        router.push('/');
+      }
     }catch(error)
     {
       console.log(error);
@@ -53,15 +59,32 @@ const AuthForm = ({type}:{type:FormType}) => {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <h2 className="text-primary-100">Preply</h2>
+          <h2 className="text-primary-100">PrepView</h2>
         </div>
         <h3>Prepare for you job interviews with AI.</h3>
       
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
-          {!isSignIn && <p>Name</p>}
-          <p>Email</p>
-          <p>Password</p>
+          {!isSignIn && <FormField 
+            control={form.control} 
+            name="name" 
+            label="Name" 
+            placeholder="Your Name"
+          />}
+          <FormField 
+            control={form.control} 
+            name="email" 
+            label="Email" 
+            placeholder="Your Email Address"
+            type="email"
+          />
+          <FormField 
+            control={form.control} 
+            name="password" 
+            label="Password" 
+            placeholder="Enter Your Password"
+            type="password"
+          />
           <Button type="submit" className="btn">{isSignIn ? "Sign in":"Create an Accoount"}</Button>
         </form>
       </Form>
--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,20 +1,30 @@
 import React from 'react'
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
+import { FormItem,FormLabel,FormControl,FormDescription,FormMessage } from './ui/form'
+import { Input } from "@/components/ui/input"
 
-const FormField = () => (
-    <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
+interface FormFieldProps <T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label:string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file"
+}
+
+
+
+const FormField = <T extends FieldValues>({control,name,label,placeholder,type="text"}:FormFieldProps<T>) => (
+    <Controller
+            name={name}
+            control={control}
+            render = {({field})=>(
               <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
+              <FormLabel className='label'>{label}</FormLabel>
+              <FormControl>
+                <Input className="input" placeholder={placeholder} type={type} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
             )}
           />
 )
