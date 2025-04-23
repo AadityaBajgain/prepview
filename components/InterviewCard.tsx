d35e79cbbc30c8ac77e63e08bd98998670fa7762
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-04-22 23:16:43 -0700

    feat: add DisplayTechIcons component and integrate it into InterviewCard

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import dayjs from 'dayjs';
 import Image from 'next/image';
 import { getRandomInterviewCover } from '@/lib/utils';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import DisplayTechIcons from './DisplayTechIcons';
 const InterviewCard = ({userId,interviewId, role, type, techstack, createdAt}:InterviewCardProps) => {
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
@@ -17,6 +20,31 @@ const InterviewCard = ({userId,interviewId, role, type, techstack, createdAt}:In
             </p>
           </div>
           <Image src={getRandomInterviewCover()} alt="cover image" width={90} height={90} className="rounded-full object-fit size-[90px]"/>
+          <h3 className='mt-5 capitalize'>
+            {role} Interview
+          </h3>
+          <div className='flex flex-row gap-5 mt-3'>
+            <div className='flex flex-row gap-2'>
+              <Image src='/calendar.svg' alt='calendar' width={22} height={22}/>
+              <p>{formattedDate}</p>
+              <div className='flex flex-row gap-2 items-center'>
+                <Image src="/star.svg" alt="star" width={22} height={22}/>
+                <p>{feedback?.totalScore || '---'}/100</p>
+
+              </div>
+            </div>
+            <p className='line-clamp-2 mt-5'>
+              {feedback?.finalAssessment || "You haven't taken interview yet. Take it to improve your skills."}
+            </p>
+          </div>
+          <div className='flex flex-row justify-between'>
+            <DisplayTechIcons techStack={techstack}/>
+            <Button className='btn-primary'>
+              <Link href={feedback?`interview/${interviewId}/feedback`:`interview/${interviewId}`}>
+                {feedback? "Check Feedback" : 'View Interview'}
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
       
