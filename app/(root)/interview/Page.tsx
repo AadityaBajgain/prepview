df202b41b2783db8b38b816714f1ca62fc3dfb87
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-04-30 10:29:44 -0700

    feat: implement Agent component for interview generation and update dependencies

--- app/(root)/interview/Page.tsx
+++ app/(root)/interview/Page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import Agent from '@/components/Agent'
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+            <h3>Interview Generation</h3>
+            <Agent userName="You" userId="user1" type="generate"/>
+    </>
   )
 }
 
